0d5312f46a5dc529f794aab493ba541febd47b1a
Anderson Silverio de Oliveira <anderson123oliveira456@gmail.com>
2026-09-24 21:45:32 -0300

    Adição do astronalta com metodo especifico

--- src/solidexercicio10/model/Astronauta.java
+++ src/solidexercicio10/model/Astronauta.java
@@ -3,20 +3,24 @@ package solidexercicio10.model;
 public class Astronauta extends Passageiro {
     public Astronauta(String nome, int x, int y) {
         super(nome, x, y);
+        
     }
 
     @Override
     public String getTipo() {
+        
         return "Astronauta";
     }
 
     @Override
     public int getPontuacao() {
+        
         return 20;
     }
 
     @Override
     public char getSimbolo() {
+        
         return 'T';
     }
 }
