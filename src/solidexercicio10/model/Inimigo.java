23922bf1451d9bcf7f01095aa843fe444e54ff5e
Anderson Silverio de Oliveira <anderson123oliveira456@gmail.com>
2026-09-24 21:48:58 -0300

    PRINCIPIO ISP

--- src/solidexercicio10/model/Inimigo.java
+++ src/solidexercicio10/model/Inimigo.java
@@ -4,15 +4,18 @@ package solidexercicio10.model;
 public class Inimigo extends EntidadeMapa implements Movel {
     public Inimigo(int x, int y) {
         super(x, y);
+        
     }
 
     @Override
     public void mover(int dx, int dy) {
         deslocar(dx, dy);
+        
     }
 
     @Override
     public char getSimbolo() {
         return 'X';
+        
     }
 }
