c98977607c6110785dd863839f48a3e4657d81aa
Anderson Silverio de Oliveira <anderson123oliveira456@gmail.com>
2026-09-24 21:52:36 -0300

    PRINCIPIO LSP

--- src/solidexercicio10/model/Professor.java
+++ src/solidexercicio10/model/Professor.java
@@ -3,20 +3,24 @@ package solidexercicio10.model;
 public class Professor extends Passageiro {
     public Professor(String nome, int x, int y) {
         super(nome, x, y);
+        
     }
 
     @Override
     public String getTipo() {
         return "Professor";
+        
     }
 
     @Override
     public int getPontuacao() {
         return 10;
+        
     }
 
     @Override
     public char getSimbolo() {
         return 'P';
+        
     }
 }
